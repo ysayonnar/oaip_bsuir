a3ce1a6a8325fd7ba8a5cc45079894c7dc6afc5a
ysayonnar <ysayonnar@gmail.com>
2024-10-14 15:17:22 +0300

    upd: зацикливание для 4 и 5 лабы

--- lab4/main.c
+++ lab4/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-//максимальное значение 978, я проверял
-#define MAX 990
+//директивы препроцессора
+//MAX для указания максимального числа n(задает длину массива чисел)
+#define MAX 1000
 
 //функция очистики потока ввода
 void clearBuffer(){
@@ -9,61 +10,74 @@ void clearBuffer(){
 	while((c = getchar()) != '\n' && c != EOF);
 }
 
-int main(int argc, char *argv[]){
-	// printf("Parametrs: ");
-	// for (int i = 0; i < argc; i++) {
-	// 	printf("\n%s", argv[i]);
-	// }
+int main(){
 	//инициализация переменных
-	int n;
+	int n = 0;
 	int inputResult = 0;
 	int numbers[MAX];
-
-	//Ввод числа n
-	while(inputResult == 0){
-		printf("Enter a number n greater than or equal to 2 and not greater than %d: ", MAX);
-		inputResult = scanf("%d", &n);
-		if(inputResult == 0){
-			printf("Incorrect input!\n");
-		}else if (n < 2 || n > MAX){ 
-			//проверка на выход за пределы границ
-			printf("n must be greater than or equal to 2 and not greater than %d!\n", MAX);
-			inputResult = 0;
-		}
-		clearBuffer();//очистка потока ввода
-	}
+	int repeatResult = 0;
 	
-	//Заполнение массива
-	for (int i = 0; i < MAX; i++){
-		//ячейки с индексам, меньшими n - 2 инициализируем нулями
-		if (i + 2 <= n){
-			numbers[i] = i + 2;
-		}else{
-			numbers[i] = 0;
+	//цикл while для  зацикливания программы
+	while (1) {
+		//Ввод числа n
+		while(n < 2 || n > MAX){
+			printf("Enter a number n greater than or equal to 2 and not greater than %d: ", MAX);
+			inputResult = scanf("%d", &n);
+			if(inputResult == 0){
+				printf("Incorrect input!\n");
+			}else if (n < 2 || n > MAX){ 
+				//проверка на выход за пределы границ
+				printf("n must be greater than or equal to 2 and not greater than %d!\n", MAX);
+				inputResult = 0;
+			}
+			clearBuffer();//очистка потока ввода
 		}
-	}
-
-	//сама работа алгоритма
-	for (int i = 0; i < n - 1; i++){
-		//чтобы не терять в производительности пропускаем нули
-		if (numbers[i] == 0){
-			continue;
-		}
-		//вложенный цикл, начинаем со следующего после i индекса(i + 1)
-		for (int j = i + 1; j < n - 1; j++){
-			//проверяем кратность и в случае если число кратно присваиваем нуль(зачеркиваем)
-			if(numbers[j] % numbers[i] == 0){
-				numbers[j] = 0;
+		
+		//Заполнение массива
+		for (int i = 0; i < MAX; i++){
+			//ячейки с индексам, меньшими n - 2 инициализируем нулями
+			if (i + 2 <= n){
+				numbers[i] = i + 2;
+			}else{
+				numbers[i] = 0;
 			}
 		}
-	}
-	//выводим только простые числа и пропускаем нули
-	printf("Prime number from 2 to %d:\t", n);
-	for (int i = 0; i < n; i++){
-		if(numbers[i] != 0){
-			printf("%d ", numbers[i]);
+
+		//сама работа алгоритма
+		for (int i = 0; i < n - 1; i++){
+			//чтобы не терять в производительности пропускаем нули
+			if (numbers[i] == 0){
+				continue;
+			}
+			//вложенный цикл, начинаем со следующего после i индекса(i + 1)
+			for (int j = i + 1; j < n - 1; j++){
+				//проверяем кратность и в случае если число кратно присваиваем нуль(зачеркиваем)
+				if(numbers[j] % numbers[i] == 0){
+					numbers[j] = 0;
+				}
+			}
+		}
+		//выводим только простые числа и пропускаем нули
+		printf("Prime number from 2 to %d:\t", n);
+		for (int i = 0; i < n; i++){
+			if(numbers[i] != 0){
+				printf("%d ", numbers[i]);
+			}
+		}
+		
+		printf("\nDo you want to continue?(1-yes): ");
+		scanf("%d", &repeatResult);
+		if (repeatResult != 1){
+			//выходим из программы
+			break;
+		}else{
+			//обнуляем переменные, массив можем не обновлять, так как он будет перезаполнен
+			n = 0;
+			inputResult = 0;
+			repeatResult = 0;
+			clearBuffer(); //очищаем поток ввода
+			continue;
 		}
 	}
-
 	return 0;
 }
--- lab5/main.c
+++ lab5/main.c
@@ -18,91 +18,106 @@ int main(void){
 	int matrix[ROWS][COLUMNS];
 	int response = 0;
 	int sum = 0;
+	int repeatResult = 0;
 
-	//даем пользователю выбор: вводить массив вручную, либо заполнить случайными числами
-	while(response == 0){
-		printf("Do you want the array to be filled with random values or to fill it manually?(1 - random/2 - manualy):");
-		scanf("%d", &response);
-		if (response != 1 && response != 2){
-			printf("Incorrect input!\n");
-			response = 0;
-		}	
-		clearBuffer(); //очищаем поток ввода
-	}
-	
-	//пользователь выбрал заполнение случайными числами
-	if (response == 1){
-		int minimal, maximum, inputResult;
-
-		
-		
-		//вводим нижнюю и верхнюю границу для случайных чисел
-		while(inputResult == 0){
-			printf("Enter the lower and upper bounds (from -10000 to 10000):");
-			inputResult = scanf("%d %d", &minimal, &maximum);
-
-			if (inputResult == 2){
-				//проверка на выход из границ
-				if (minimal < -10000 || maximum > 10000){
-					printf("Out of Bonds!\n");
-					inputResult = 0;
-				}
-				//проверка на то чтобы нижняя граница не была больше верхней
-				if(minimal >= maximum){
-					printf("Minimal cant be greater than maximum!\n");
-					inputResult = 0;
-				}
-			}else{
+	//цикл while для зацикливания программы
+	while(1){
+		//даем пользователю выбор: вводить массив вручную, либо заполнить случайными числами
+		while(response == 0){
+			printf("Do you want the array to be filled with random values or to fill it manually?(1 - random/2 - manualy):");
+			scanf("%d", &response);
+			if (response != 1 && response != 2){
 				printf("Incorrect input!\n");
-				inputResult = 0;
-			}
-			clearBuffer();//очищаем поток ввода
+				response = 0;
+			}	
+			clearBuffer(); //очищаем поток ввода
 		}
 
-		srand(time(0));//создаем seed для генерации случайных чисел по времени, иначе при каждом запуску числа будут одинаковые
-		for (int i = 0; i < ROWS; i++){
+		//пользователь выбрал заполнение случайными числами
+		if (response == 1){
+			int minimal = 0, maximum = 0, inputResult = 0;
+			//вводим нижнюю и верхнюю границу для случайных чисел
+			while(inputResult == 0){
+				printf("Enter the lower and upper bounds (from -10000 to 10000):");
+				inputResult = scanf("%d %d", &minimal, &maximum);
+				// printf("\nminimal: %d, maximum: %d\n", minimal, maximum);
+				if (inputResult == 2){
+					//проверка на выход из границ
+					if (minimal < -10000 || maximum > 10000){
+						printf("Out of Bonds!\n");
+						inputResult = 0;
+					}
+					//проверка на то чтобы нижняя граница не была больше верхней
+					if(minimal >= maximum){
+						printf("Minimal cant be greater than maximum!\n");
+						inputResult = 0;
+					}
+				}else{
+					printf("Incorrect input!\n");
+					inputResult = 0;
+				}
+				clearBuffer();//очищаем поток ввода
+			}
+
+			srand(time(0));//создаем seed для генерации случайных чисел по времени, иначе при каждом запуску числа будут одинаковые
+			for (int i = 0; i < ROWS; i++){
+				for (int j = 0; j < COLUMNS; j++){
+					//заполняем массив случайными числами
+					matrix[i][j] = minimal + rand() % (maximum - minimal + 1);
+				}			
+			}
+		}
+		
+		//вариант заполнения вручную
+		if(response == 2){
+			int inputInfo;
+			for (int i = 0; i < ROWS; i++){
+				for (int j = 0; j < COLUMNS; j++){
+					inputInfo = 0;
+					while(inputInfo != 1){
+						printf("Enter element with indexes %dx%d: ",i + 1, j + 1);
+						inputInfo = scanf("%d", &matrix[i][j]);
+						if(inputInfo != 1){
+							printf("Invalid input!\n");
+							clearBuffer();//очищаем поток ввода
+						}
+					}
+				}
+			}
+		}
+
+		printf("Array looks like:\n");
+
+		//вывод массива и счет суммы 
+		for(int i = 0; i < ROWS; i++){
+			printf("|");
 			for (int j = 0; j < COLUMNS; j++){
-				//заполняем массив случайными числами
-				matrix[i][j] = minimal + rand() % (maximum - minimal + 1);
-			}			
-		}
-	}
-	
-	//вариант заполнения вручную
-	if(response == 2){
-		int inputInfo;
-		for (int i = 0; i < ROWS; i++){
-            for (int j = 0; j < COLUMNS; j++){
-                inputInfo = 0;
-                while(inputInfo != 1){
-                    printf("Enter element with indexes %dx%d: ",i + 1, j + 1);
-                    inputInfo = scanf("%d", &matrix[i][j]);
-                    if(inputInfo != 1){
-                        printf("Invalid input!\n");
-                        clearBuffer();//очищаем поток ввода
-                    }
-                }
-            }
-        }
-	}
-
-	printf("Array looks like:\n");
-
-	//вывод массива и счет суммы 
-	for(int i = 0; i < ROWS; i++){
-		printf("|");
-		for (int j = 0; j < COLUMNS; j++){
-			printf(" %5d ", matrix[i][j]);
-			//условие для суммирования, два амперсанта можно заменить на логическое или, тк в условии не совсем точно сказано
-			if(matrix[i][j] % 2 != 0 && (i + j) % 2 == 0){
-				sum += matrix[i][j];
+				printf(" %5d ", matrix[i][j]);
+				//условие для суммирования, два амперсанта можно заменить на логическое или, тк в условии не совсем точно сказано
+				if(matrix[i][j] % 2 != 0 && (i + j) % 2 == 0){
+					sum += matrix[i][j];
+				}
 			}
+			printf("|\n");
 		}
-		printf("|\n");
-	}
-	
-	//выводим сумму
-	printf("The sum of the elements having odd values and standing in even places is %d", sum);
+		
+		//выводим сумму
+		printf("The sum of the elements having odd values and standing in even places is %d", sum);
 
-	return 0;
+
+		//спрашиваем у пользователя желает ли он продолжить выполнение
+		printf("\nDo you want to continue?(1-yes): ");
+		scanf("%d", &repeatResult);
+		if (repeatResult != 1){
+			//выходим из программы
+			break;
+		}else{
+			//обнуляем переменные, массив можем не обновлять, так как он будет перезаполнен
+			response = 0;
+			sum = 0;
+			repeatResult = 0;
+			clearBuffer(); //очищаем поток ввода
+			continue;
+		}
+	}
 }
