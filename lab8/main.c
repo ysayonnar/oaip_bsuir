db8ef82d72b32a1f4747c1ce76fdec22f6706984
ysayonnar <ysayonnar@gmail.com>
2024-12-06 23:51:52 +0300

    upd: lab8

--- lab8/main.c
+++ lab8/main.c
@@ -2,12 +2,13 @@
 #include "functions.h"
 
 int main(void){
-	int n = inputNumber("Enter maximum string length: ", 1, 100); //ввод длины строки
-	char *str = inputString(n); //ввод строки
-	printString("Before: ", str);
-	str = transformString(str, &n);//преобразование строки
-	printString("After: ", str);
-	free(str);
-}
+		int n = inputNumber("Enter maximum string length: ", 1, 100); //ввод длины строки
+		char *str = inputString(n); //ввод строки
+		printString("Before: ", str);
+		str = transformString(str, &n);//преобразование строки
+		printString("After: ", str);
+		free(str);
 
-//gcc main.c functions.c -o main
+		// printf("Do you want to continue(1 - yes)?: ");
+		// scanf("%d", &userResponse);
+}
